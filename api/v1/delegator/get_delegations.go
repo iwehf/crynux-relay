ddde6e7931dce9850eccb1a93e1e9b4eef006e6f
iwehf <henry.lee@crynux.ai>
2025-12-01 17:25:41 +0800

    add page and page size params to get delegations and get delegated nodes api

--- api/v1/delegator/get_delegations.go
+++ api/v1/delegator/get_delegations.go
@@ -1,12 +1,12 @@
 package delegator
 
 import (
+	"context"
 	"crynux_relay/api/v1/response"
 	"crynux_relay/config"
 	"crynux_relay/models"
 	"errors"
 	"math/big"
-	"sort"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -16,6 +16,8 @@ import (
 type GetDelegationsInput struct {
 	UserAddress string `json:"user_address" path:"user_address" description:"address of the delegator" validate:"required"`
 	Network     string `json:"network" query:"network" description:"network of the delegator" validate:"required"`
+	Page        int    `json:"page" query:"page" description:"The page" default:"1"`
+	PageSize    int    `json:"page_size" query:"page_size" description:"The page size" default:"30"`
 }
 
 type DelegationInfo struct {
@@ -28,13 +30,49 @@ type DelegationInfo struct {
 	TodayEarnings models.BigInt `json:"today_earnings"`
 }
 
+type DelegationsResult struct {
+	Delegations []DelegationInfo `json:"delegations"`
+	Total       int64            `json:"total"`
+}
+
 type GetDelegationsOutput struct {
 	response.Response
-	Data []DelegationInfo `json:"data"`
+	Data *DelegationsResult `json:"data"`
+}
+
+func getDelegationsOfUser(ctx context.Context, db *gorm.DB, userAddress string, network *string, offset, limit int) ([]models.Delegation, int64, error) {
+	dbCtx, cancel := context.WithTimeout(ctx, 5*time.Second)
+	defer cancel()
+
+	var userStakings []models.Delegation
+	dbi := db.WithContext(dbCtx).Model(&models.Delegation{}).Where("delegator_address = ?", userAddress).Where("valid = ?", true)
+	if network != nil {
+		dbi = dbi.Where("network = ?", network)
+	}
+
+	var total int64
+	if err := dbi.Count(&total).Error; err != nil {
+		return nil, 0, err
+	}
+
+	if err := dbi.Order("updated_at DESC").Offset(offset).Limit(limit).Find(&userStakings).Error; err != nil {
+		return nil, 0, err
+	}
+	return userStakings, 0, nil
 }
 
 func GetDelegations(c *gin.Context, input *GetDelegationsInput) (*GetDelegationsOutput, error) {
-	userStakings, err := models.GetDelegationsOfUser(c.Request.Context(), config.GetDB(), input.UserAddress, &input.Network)
+	page := 1
+	if input.Page > 0 {
+		page = input.Page
+	}
+	pageSize := 30
+	if input.PageSize > 0 {
+		pageSize = input.PageSize
+	}
+	offset := (page - 1) * pageSize
+	limit := pageSize
+	userStakings, total, err := getDelegationsOfUser(c.Request.Context(), config.GetDB(), input.UserAddress, &input.Network, offset, limit)
 	if err != nil {
 		return nil, response.NewExceptionResponse(err)
 	}
@@ -97,11 +135,10 @@ func GetDelegations(c *gin.Context, input *GetDelegationsInput) (*GetDelegations
 		})
 	}
 
-	sort.Slice(res, func(i, j int) bool {
-		return res[i].TotalEarnings.Cmp(&res[j].TotalEarnings.Int) > 0
-	})
-
 	return &GetDelegationsOutput{
-		Data: res,
+		Data: &DelegationsResult{
+			Delegations: res,
+			Total:       total,
+		},
 	}, nil
 }
